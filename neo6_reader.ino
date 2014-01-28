22a50ccb3b02e052e9d80472ce7d80824cefa57e
Skip Tabor <pneusolematic@mac.com>
2014-01-28 15:12:25 -0500

    Added a bunch of functions

--- neo6_reader.ino
+++ neo6_reader.ino
@@ -1,19 +1,19 @@
 #define GPRMC_TERM "$GPRMS,"
 
 char nmeaSentence[68];
+String latitude;
+String longitude;
+String lndSpeed;
+String gpsTime;
 
 void setup()
 {
   Serial.begin(115200);
-  //ss.begin(4800);
   Serial2.begin(9600);
 }
 
 void loop()
 {
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
 
   // For one second we parse GPS data and report some key values
   for (unsigned long start = millis(); millis() - start < 1000;)
@@ -21,16 +21,217 @@ void loop()
     while (Serial2.available())
     {
       char c = Serial2.read();
-      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+
       switch(c)
       {
-        //case '\r': //do nothing
-        //case '\n': //do nothing
-        case '$':
-          Serial2.readBytesUntil(0x2A, nmeaSentence, 67);
-          Serial.println(nmeaSentence);
+      case '$':
+        Serial2.readBytesUntil(0x2A, nmeaSentence, 67);
+        //Serial.println(nmeaSentence);
+        latitude = parseGprmcLat(nmeaSentence);
+        longitude = parseGprmcLon(nmeaSentence);
+        lndSpeed = parseGprmcSpeed(nmeaSentence);
+        gpsTime = parseGprmcTime(nmeaSentence);
+
+        if(latitude > "")
+        {
+          Serial.println("latitude: " + latitude);
+        }
+
+        if(longitude > "")
+        {
+          Serial.println("longitude: " + longitude);
+        }  
+
+        if(lndSpeed > "")
+        {
+          Serial.println("Speed (knots): " + lndSpeed);
+        }
+
+        if(lndSpeed > "")
+        {
+          Serial.println("Time of Fix: " + gpsTime.toInt() - 5);
+        }           
       }
     }
+  }
+}
+
+//Parse GPRMC NMEA sentence data from String
+//String must be GPRMC or no data will be parsed
+//Return Latitude
+String parseGprmcLat(String s)
+{
+  int pLoc = 0; //paramater location pointer
+  int lEndLoc = 0; //lat parameter end location
+  int dEndLoc = 0; //direction parameter end location
+  String lat;
+
+  /*make sure that we are parsing the GPRMC string. 
+   Found that setting s.substring(0,5) == "GPRMC" caused a FALSE.
+   There seemed to be a 0x0D and 0x00 character at the end. */
+  if(s.substring(0,4) == "GPRM")
+  {
+    //Serial.println(s);
+    for(int i = 0; i < 5; i++)
+    {
+      if(i < 3) 
+      {
+        pLoc = s.indexOf(',', pLoc+1);
+        /*Serial.print("i < 3, pLoc: ");
+         Serial.print(pLoc);
+         Serial.print(", ");
+         Serial.println(i);*/
+      }
+      if(i == 3)
+      {
+        lEndLoc = s.indexOf(',', pLoc+1);
+        lat = s.substring(pLoc+1, lEndLoc);
+        /*Serial.print("i = 3, pLoc: ");
+         Serial.println(pLoc);
+         Serial.print("lEndLoc: ");
+         Serial.println(lEndLoc);*/
+      }
+      else
+      {
+        dEndLoc = s.indexOf(',', lEndLoc+1);
+        lat = lat + " " + s.substring(lEndLoc+1, dEndLoc);
+        /*Serial.print("i = 4, lEndLoc: ");
+         Serial.println(lEndLoc);
+         Serial.print("dEndLoc: ");
+         Serial.println(dEndLoc);*/
+      }
+    }
+    return lat; 
+  }
+}
+
+//Parse GPRMC NMEA sentence data from String
+//String must be GPRMC or no data will be parsed
+//Return Longitude
+String parseGprmcLon(String s)
+{
+  int pLoc = 0; //paramater location pointer
+  int lEndLoc = 0; //lat parameter end location
+  int dEndLoc = 0; //direction parameter end location
+  String lon;
+
+  /*make sure that we are parsing the GPRMC string. 
+   Found that setting s.substring(0,5) == "GPRMC" caused a FALSE.
+   There seemed to be a 0x0D and 0x00 character at the end. */
+  if(s.substring(0,4) == "GPRM")
+  {
+    //Serial.println(s);
+    for(int i = 0; i < 7; i++)
+    {
+      if(i < 5) 
+      {
+        pLoc = s.indexOf(',', pLoc+1);
+        /*Serial.print("i < 3, pLoc: ");
+         Serial.print(pLoc);
+         Serial.print(", ");
+         Serial.println(i);*/
+      }
+      if(i == 5)
+      {
+        lEndLoc = s.indexOf(',', pLoc+1);
+        lon = s.substring(pLoc+1, lEndLoc);
+        /*Serial.print("i = 3, pLoc: ");
+         Serial.println(pLoc);
+         Serial.print("lEndLoc: ");
+         Serial.println(lEndLoc);*/
+      }
+      else
+      {
+        dEndLoc = s.indexOf(',', lEndLoc+1);
+        lon = lon + " " + s.substring(lEndLoc+1, dEndLoc);
+        /*Serial.print("i = 4, lEndLoc: ");
+         Serial.println(lEndLoc);
+         Serial.print("dEndLoc: ");
+         Serial.println(dEndLoc);*/
+      }
+    }
+    return lon; 
+  }
+}
+
+//Parse GPRMC NMEA sentence data from String
+//String must be GPRMC or no data will be parsed
+//Return Longitude
+String parseGprmcSpeed(String s)
+{
+  int pLoc = 0; //paramater location pointer
+  int lEndLoc = 0; //lat parameter end location
+  int dEndLoc = 0; //direction parameter end location
+  String lndSpeed;
+
+  /*make sure that we are parsing the GPRMC string. 
+   Found that setting s.substring(0,5) == "GPRMC" caused a FALSE.
+   There seemed to be a 0x0D and 0x00 character at the end. */
+  if(s.substring(0,4) == "GPRM")
+  {
+    //Serial.println(s);
+    for(int i = 0; i < 8; i++)
+    {
+      if(i < 7) 
+      {
+        pLoc = s.indexOf(',', pLoc+1);
+        /*Serial.print("i < 8, pLoc: ");
+         Serial.print(pLoc);
+         Serial.print(", ");
+         Serial.println(i);*/
+      }
+      else
+      {
+        lEndLoc = s.indexOf(',', pLoc+1);
+        lndSpeed = s.substring(pLoc+1, lEndLoc);
+        /*Serial.print("i = 8, pLoc: ");
+         Serial.println(pLoc);
+         Serial.print("lEndLoc: ");
+         Serial.println(lEndLoc);*/
+      }
+    }
+    return lndSpeed; 
+  }
+}
+
+
+//Parse GPRMC NMEA sentence data from String
+//String must be GPRMC or no data will be parsed
+//Return Longitude
+String parseGprmcTime(String s)
+{
+  int pLoc = 0; //paramater location pointer
+  int lEndLoc = 0; //lat parameter end location
+  int dEndLoc = 0; //direction parameter end location
+  String gpsTime;
+
+  /*make sure that we are parsing the GPRMC string. 
+   Found that setting s.substring(0,5) == "GPRMC" caused a FALSE.
+   There seemed to be a 0x0D and 0x00 character at the end. */
+  if(s.substring(0,4) == "GPRM")
+  {
+    //Serial.println(s);
+    for(int i = 0; i < 2; i++)
+    {
+      if(i < 1) 
+      {
+        pLoc = s.indexOf(',', pLoc+1);
+        /*Serial.print("i < 8, pLoc: ");
+         Serial.print(pLoc);
+         Serial.print(", ");
+         Serial.println(i);*/
+      }
+      else
+      {
+        lEndLoc = s.indexOf(',', pLoc+1);
+        gpsTime = s.substring(pLoc+1, lEndLoc);
+        /*Serial.print("i = 8, pLoc: ");
+         Serial.println(pLoc);
+         Serial.print("lEndLoc: ");
+         Serial.println(lEndLoc);*/
+      }
+    }
+    return gpsTime; 
   }
 }
 
@@ -38,39 +239,13 @@ void loop()
 String charToString(char *c)
 {
 
- String val = "";
+  String val = "";
 
- for(int i = 0; i <= sizeof(c); i++) 
- {
-   val = val + c[i];
- }
- 
-return val;
-}
-
-//Parse GPRMC NMEA sentence data from String
-//String must be GPRMC or no data will be parsed
-String parseGprmcLon(String s)
-{
-  int delimCntr = 0; //count number or delimiters
-  int pLoc = 0; //paramater location pointer
-  int pEndLoc = 0; //parameter end location
-  String lon;
-  if(s.substring(0,4) == 'GPRMC')
+  for(int i = 0; i <= sizeof(c); i++) 
   {
-    for(int i = 0; i < 3; i++)
-    {
-      if(delimCntr < 2) 
-      {
-        pLoc = s.indexOf(',', pLoc+1);
-      }
-      else
-      {
-        pEndLoc = s.indexOf(',', pLoc+1);
-        lon = s.substring(pLoc+1, pEndLoc);
-      }
-      return lon;
-    }
-      
+    val = val + c[i];
   }
+
+  return val;
 }
+
